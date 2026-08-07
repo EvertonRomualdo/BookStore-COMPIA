a043c2b21049bdfc85bd19959af163049b4200f0
Everton <evertonrdaniel@gmail.com>
2026-08-06 23:28:19 -0300

    feat: add aboult page implementation

--- frontend/Compia/src/App.tsx
+++ frontend/Compia/src/App.tsx
@@ -6,16 +6,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginForm from './components/LoginForm/LoginForm'
 import CadasterPage from './pages/CadasterPage'
 import HomePage from './pages/HomePage'
+import AboutPage from "./pages/AboutPage.tsx";
 function App() {
 
   //Rotas das paginas
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
+        <Route path='/' element={<AboutPage/>}/>
         <Route path='/home' element={<HomePage/>}/>
         <Route path='/login' element={<LoginForm/>}/>
         <Route path='/register' element={<CadasterPage/>}/>
+        <Route path='/aboult' element={<AboutPage/>}/>
         <Route path='*' element={<><h1>Not Found</h1></>}/>
       </Routes>
     </BrowserRouter>
